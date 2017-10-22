56377895e67d8973fcec2757dff2594912ba3651
Amir Makanvand <makanvaa@aston.ac.uk>
2017-10-22 04:54:30 +0100

    Added a class Receipt to hold the data, Main to run

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+
+
+
+public class Main {
+	
+	ArrayList receipt = new ArrayList<Receipt>();
+	private int budget = 0;
+	//Create an ArrayList
+	
+	private void add(float price, String description){
+		receipt.add(price, description);
+		
+		int last = receipt.size();
+		
+		
+		
+	}
+	
+	
+	
+	//insert into ArrayList
+
+	//get ArrayList
+	
+	//Set ArrayList
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ src/Receipt.java
@@ -0,0 +1,28 @@
+
+public class Receipt {
+	
+	private float price;
+	private String description;
+	
+	public void Receipt(float price, String description){
+		this.price = price;
+		this.description = description;
+	}
+	
+	public float getPrice(){
+		return price;
+	}
+	
+	public void setPrice(float p){
+		price = p;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setDescription(String d){
+		description = d;
+	}
+	
+}
